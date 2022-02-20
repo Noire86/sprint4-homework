9a4504c885e9a113bc9226132ddc00952279a159
Noire86 <gleb.iwanow2010@yandex.ru>
2022-02-20 22:23:21 +0500

    [Feature] Implemented StatusHelper

---
+++ src/ru/soular/taskmanager/util/StatusHelper.java
@@ -0,0 +1,32 @@
+package ru.soular.taskmanager.util;
+
+import ru.soular.taskmanager.models.Epic;
+import ru.soular.taskmanager.models.SubTask;
+import ru.soular.taskmanager.models.Task.TaskState;
+
+import java.util.List;
+
+public class StatusHelper {
+
+    private final Epic epic;
+    private final List<SubTask> subTasks;
+
+    public StatusHelper(Epic epic) {
+        this.epic = epic;
+        this.subTasks = epic.getSubTasks();
+    }
+
+    public TaskState updateEpicStatus() {
+        if ((epic.getSubTasks().size() == 0) || checkSameStatus(TaskState.NEW)) {
+            return TaskState.NEW;
+        } else if (checkSameStatus(TaskState.DONE)) {
+            return TaskState.DONE;
+        } else {
+            return TaskState.IN_PROGRESS;
+        }
+    }
+
+    private boolean checkSameStatus(TaskState status) {
+        return subTasks.stream().allMatch(s -> s.getStatus().equals(status));
+    }
+}
