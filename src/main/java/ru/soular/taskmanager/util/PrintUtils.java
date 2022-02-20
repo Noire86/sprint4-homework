5e85eb2c9e387493544928d760d4bfeb60bf62ae
Noire86 <gleb.iwanow2010@yandex.ru>
2022-02-21 02:19:05 +0500

    [Feature] PrintUtils implemented

---
+++ src/main/java/ru/soular/taskmanager/util/PrintUtils.java
@@ -0,0 +1,41 @@
+package ru.soular.taskmanager.util;
+
+import ru.soular.taskmanager.managers.TaskManager;
+import ru.soular.taskmanager.models.Epic;
+import ru.soular.taskmanager.models.SubTask;
+import ru.soular.taskmanager.models.Task;
+
+import java.util.List;
+import java.util.Map;
+
+public class PrintUtils {
+
+    public static final String DELIMITER = "---------------------------";
+
+    public static <T extends Task> void printTasks(List<T> tasks) {
+        tasks.forEach(t -> {
+            if (t.getClass() == Task.class) {
+                System.out.printf("Обычная задача - %s  | ID: %d | Статус: %s | Описание: %s%n",
+                        t.getName(), t.getId(), t.getStatus(), t.getDescription());
+
+            } else if (t.getClass() == Epic.class) {
+                System.out.printf("Epic задача - %s  | ID: %d | Статус: %s | Описание: %s | Кол-во субзадач: %d%n",
+                        t.getName(), t.getId(), t.getStatus(), t.getDescription(), TaskManager.getInstance().getSubTasks((Epic) t).size());
+
+            } else if (t.getClass() == SubTask.class) {
+                System.out.printf("Cубзадача - %s  | ID: %d | Статус: %s | Описание: %s | Имя и ID эпика: %s:%d%n",
+                        t.getName(), t.getId(), t.getStatus(), t.getDescription(), ((SubTask) t).getParentEpic().getName(), ((SubTask) t).getParentEpic().getId());
+
+            }
+        });
+        System.out.println(DELIMITER);
+    }
+
+    public static void printAllTasks(Map<Integer, Task> tasksMap) {
+        tasksMap.forEach((key, value) ->
+                System.out.printf("ID Задачи: %d | Имя задачи: %s | Тип задачи: %s%n",
+                key, value.getName(), value.getClass().getSimpleName()));
+
+        System.out.println(DELIMITER);
+    }
+}
