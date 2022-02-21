c47d49a899fff76b042ebefb95fabd0977b86602
Noire86 <gleb.iwanow2010@yandex.ru>
2022-02-21 16:07:35 +0500

    [Refactor] Removed StatusHelper, transferred status update to Epic

--- src/main/java/ru/soular/taskmanager/models/Epic.java
+++ src/main/java/ru/soular/taskmanager/models/Epic.java
@@ -1,6 +1,5 @@
 package ru.soular.taskmanager.models;
 
-import ru.soular.taskmanager.helpers.StatusHelper;
 import ru.soular.taskmanager.managers.TaskManager;
 
 import java.util.List;
@@ -11,12 +10,10 @@ import java.util.List;
  * желающих установить свой статус для эпика
  */
 public class Epic extends Task {
-    private final StatusHelper statusHelper;
     private final TaskManager taskManager;
 
     public Epic(String name, String description) {
         super(name, description);
-        statusHelper = new StatusHelper(this);
         taskManager = TaskManager.getInstance();
     }
 
@@ -26,6 +23,31 @@ public class Epic extends Task {
 
     @Override
     public void setStatus(TaskState status) {
-        super.setStatus(statusHelper.updateEpicStatus());
+        super.setStatus(updateEpicStatus());
+    }
+
+    /**
+     * Метод, проверяющий связанные с эпиком субтаски.
+     * Если список пустой - NEW
+     * Если у всех субтасков один статус - делаем такой же для эпика.
+     * Иначе IN_PROGRESS
+     */
+    private TaskState updateEpicStatus() {
+        if ((taskManager.getSubTasks(this).size() == 0) || checkSameStatus(TaskState.NEW)) {
+            return TaskState.NEW;
+
+        } else if (checkSameStatus(TaskState.DONE)) {
+            return TaskState.DONE;
+
+        } else {
+            return TaskState.IN_PROGRESS;
+        }
+    }
+
+    /**
+     * Красивая лямбда для уменьшения повторяемых итераций
+     */
+    private boolean checkSameStatus(TaskState status) {
+        return taskManager.getSubTasks(this).stream().allMatch(s -> s.getStatus().equals(status));
     }
 }
